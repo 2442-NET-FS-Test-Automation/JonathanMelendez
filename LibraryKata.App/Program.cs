47a0c23db38f1048e67c2170721dc408359cf44b
Jonnhy <104951133+qJona65p@users.noreply.github.com>
2026-06-11 20:00:23 -0600

    Added LibraryKata Notes

---
+++ LibraryKata.App/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace LibraryKata.App;
+
+public class Program
+{
+    public static void Main()
+    {
+        DataTypesAndoperators();
+    }
+
+    private static void DataTypesAndoperators()
+    {
+        Console.WriteLine("=== Data types and Operators ===");
+
+        // Primitives
+        // integer          byte, short, int, uint, long, ulong
+        // floating point   float, double, decimal
+        // boolean          bool (true, false)        
+        // character        char
+        // string
+
+        // Operators
+        // Same as c++
+
+        // No pointers yay
+
+        // Formated string (python-like)
+        // $""
+    }
+}
